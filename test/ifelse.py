91652943a6aa1186bb0721eb5ae2bc82bf59eb90
Margatroid <108602380+M4rg4tr01d@users.noreply.github.com>
2024-04-24 12:03:50 +0800

    update if_else

---
+++ test/ifelse.py
@@ -0,0 +1,16 @@
+a = 15
+if a <100:
+    if not a > 10:
+        print("a <= 10")
+    elif a == 11:
+        print("a == 11")
+    elif a == 10:
+        print("a==10")
+    else:
+        print("a > 11")
+else:
+    b = 0
+    if b == 0 :
+        print("b==0")
+    print("a > 100")
+print(a)
