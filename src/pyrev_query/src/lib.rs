d79e7773d9e2f33b7f4a34dbd94d2c57be4c7837
HACBIT <107751081+hacbit@users.noreply.github.com>
2024-07-22 16:06:33 +0800

    Separate the definition of QueryId into a separate crate pyrev_query_inner and make it a dependency of pyrev_query and pyrev_ast

--- src/pyrev_query/src/lib.rs
+++ src/pyrev_query/src/lib.rs
@@ -2,52 +2,12 @@
 //! And provides the safety query for the expressions.
 
 use pyrev_ast::*;
+use pyrev_query_inner::*;
 use std::{
     any::TypeId,
     collections::{HashMap, HashSet},
-    marker::PhantomData,
 };
 
-/// QueryId is a marker for a query. It can be an integer or a string.
-///
-/// Each data is associated with a query id in the DataMap.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct QueryId {
-    /// The id of the query.
-    id: usize,
-    /// A type marker.
-    type_id: *mut TypeId,
-    /// To mark the ownership of the type_id.
-    _marker: PhantomData<*mut TypeId>,
-}
-
-impl QueryId {
-    /// Create a new QueryId instance.
-    ///
-    /// # Safety
-    #[inline]
-    pub(crate) fn new(id: usize, type_id: TypeId) -> Self {
-        QueryId {
-            id,
-            type_id: unsafe { std::mem::transmute(Box::new(type_id)) },
-            _marker: PhantomData,
-        }
-    }
-
-    /// Get the type id of the query id.
-    #[inline]
-    pub fn type_id(&self) -> TypeId {
-        unsafe { *self.type_id }
-    }
-
-    /// Update the type id of the query id.
-    #[inline]
-    pub fn set_type_id(&self, type_id: TypeId) {
-        unsafe {
-            *self.type_id = type_id;
-        }
-    }
-}
 
 /// Holds all the datas and their associated query ids.
 #[derive(Debug)]
