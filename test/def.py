42cc7275ba7a458067928805520f53210824f565
HACBIT <107751081+hacbit@users.noreply.github.com>
2024-08-13 09:42:35 +0800

    fixed `def` test

--- test/def.py
+++ test/def.py
@@ -6,6 +6,6 @@ def foo(a: int, b: int = 2, c = 3):
     print("this is foo")
     return a, b, c
 b = lambda a: a + 1
-[i + 1 for i in range(10)]
+arr = [i + 1 for i in range(10)]
 a = 1
 foo(test(a, 2), c = 5)
